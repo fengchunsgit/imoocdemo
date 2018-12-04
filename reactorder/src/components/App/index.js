166e09145bfc6d903c7b91109a720eee5d051374
fengchunsgit <1055688803@qq.com>
2018-12-04 18:12:31 +0800

    add header

--- reactorder/src/components/App/index.js
+++ reactorder/src/components/App/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './style.css';
 import OrderList from '../OrderList';
+import Header from '../Header';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="app">
+        <Header/>
         <OrderList/>
       </div>
     );
--- reactorder/src/components/OrderList/index.js
+++ reactorder/src/components/OrderList/index.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react';
 import OrderItem from '../OrderItem';
 
-const data={
+const data=[{
   id:1,
   shop:"昌邑菜",
   picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
   product:"百香果",
   price:19.9,
   ifCommented:true,
+},
+{
+  id:2,
+  shop:"昌邑菜",
+  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
+  product:"百香果",
+  price:29.9,
+  ifCommented:true,
+},{
+  id:3,
+  shop:"昌邑菜",
+  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
+  product:"百香果",
+  price:39.9,
+  ifCommented:true,
+},{
+  id:4,
+  shop:"昌邑菜",
+  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
+  product:"百香果",
+  price:49.9,
+  ifCommented:true,
 }
+]
 
 class OrderList extends Component {
   render() {
     return (
       <div className="OrderList">
-        <OrderItem data={data}/>
+      {
+        data.map(item=>{
+          return <OrderItem key={item.id} data={item}/>
+        })
+      }
+        
       </div>
     );
   }
