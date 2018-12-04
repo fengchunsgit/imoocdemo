1f9957092d31db45b2c59359b0e7d37c1f9babc8
fengchunsgit <1055688803@qq.com>
2018-12-04 18:25:25 +0800

    mock data

--- reactorder/src/components/OrderList/index.js
+++ reactorder/src/components/OrderList/index.js
@@ -1,44 +1,28 @@
 import React, { Component } from 'react';
 import OrderItem from '../OrderItem';
 
-const data=[{
-  id:1,
-  shop:"昌邑菜",
-  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
-  product:"百香果",
-  price:19.9,
-  ifCommented:true,
-},
-{
-  id:2,
-  shop:"昌邑菜",
-  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
-  product:"百香果",
-  price:29.9,
-  ifCommented:true,
-},{
-  id:3,
-  shop:"昌邑菜",
-  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
-  product:"百香果",
-  price:39.9,
-  ifCommented:true,
-},{
-  id:4,
-  shop:"昌邑菜",
-  picture:"https://p1.meituan.net/210.0/wmproductdwm/1edba22e8c933ee751e1279909273446252034.jpg",
-  product:"百香果",
-  price:49.9,
-  ifCommented:true,
-}
-]
-
 class OrderList extends Component {
+
+  constructor(props){
+    super(props);
+    this.state={data:[]};
+  }
+  componentDidMount(){
+    fetch('/mock/orders.json').then(res=>{
+      if(res.ok){
+        res.json().then(data=>{
+          this.setState({
+            data
+          })
+        })
+      }
+    })
+  }
   render() {
     return (
       <div className="OrderList">
       {
-        data.map(item=>{
+        this.state.data.map(item=>{
           return <OrderItem key={item.id} data={item}/>
         })
       }
