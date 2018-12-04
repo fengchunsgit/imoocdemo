1652448b3b86a8e42d1ab3342344f704776b4aa0
fengchunsgit <1055688803@qq.com>
2018-12-04 16:44:55 +0800

    header orderlist orderitem

--- reactorder/src/components/App/index.js
+++ reactorder/src/components/App/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-hello
+        hello
       </div>
     );
   }
